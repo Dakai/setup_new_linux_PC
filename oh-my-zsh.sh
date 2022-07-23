80435034a052a93c030b5142eb82d4e9021ce086
Dakai <oudakai@gmail.com>
2022-07-24 02:36:10 +0800

    oh-my-zsh

---
+++ oh-my-zsh.sh
@@ -0,0 +1,7 @@
+sudo dnf install -y dnf
+sudo usermod -s $(which zsh) dakai
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+ln ./.zshrc ~/.zshrc
