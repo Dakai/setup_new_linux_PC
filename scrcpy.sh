88ed4904085198ddd881a212dcbd98cad7de0546
Dakai <oudakai@gmail.com>
2022-08-26 01:30:50 +0800

    no power off when scrcpy exits

--- scrcpy.sh
+++ scrcpy.sh
@@ -1,2 +1,3 @@
 export SDL_VIDEODRIVER=wayland
-scrcpy -b 4M -Sw -e --encoder 'OMX.google.h264.encoder' --power-off-on-close --window-width 400 --window-height 710 &
+#scrcpy -b 4M -Sw -e --encoder 'OMX.google.h264.encoder' --power-off-on-close --window-width 400 --window-height 710 &
+scrcpy -b 4M -Sw -e --encoder 'OMX.hisi.video.encoder.avc' --window-width 400 --window-height 710 &
