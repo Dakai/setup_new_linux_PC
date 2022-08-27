22a611a3dd34dcba4e1eb377a8f3229107f963e6
Dakai <oudakai@gmail.com>
2022-08-27 14:16:18 +0800

    scrcpy X11

--- scrcpy.sh
+++ scrcpy.sh
@@ -1,3 +1,9 @@
-export SDL_VIDEODRIVER=wayland
-#scrcpy -b 4M -Sw -e --encoder 'OMX.google.h264.encoder' --power-off-on-close --window-width 400 --window-height 710 &
-scrcpy -b 4M -Sw -e --encoder 'OMX.hisi.video.encoder.avc' --window-width 400 --window-height 710 &
+#export SDL_VIDEODRIVER=wayland 
+#Wayland is still buggy
+
+export SDL_VIDEODRIVER=X11
+
+#scrcpy -b 4M -Sw -e --encoder 'OMX.google.h264.encoder' --window-width 400 --window-height 710 &
+# google encoder is slow in responsiveness.
+
+scrcpy -b 4M -Sw -e --encoder 'OMX.hisi.video.encoder.avc' --window-width 400 --window-height 710 
