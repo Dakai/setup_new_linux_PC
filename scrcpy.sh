79ddfbad1b350567a6dc1ff1a7b06cb0e4b05c20
Dakai <oudakai@gmail.com>
2022-08-27 14:19:31 +0800

    scrcpy tweak

--- scrcpy.sh
+++ scrcpy.sh
@@ -6,4 +6,4 @@ export SDL_VIDEODRIVER=X11
 #scrcpy -b 4M -Sw -e --encoder 'OMX.google.h264.encoder' --window-width 400 --window-height 710 &
 # google encoder is slow in responsiveness.
 
-scrcpy -b 4M -Sw -e --encoder 'OMX.hisi.video.encoder.avc' --window-width 400 --window-height 710 
+scrcpy -b 8M -Sw -e --encoder 'OMX.hisi.video.encoder.avc' --window-width 800 --window-height 1420 
