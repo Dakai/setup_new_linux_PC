b80730c57524fce86081a843e4f9292d3d119baa
dakai <oudakai@gmail.com>
2023-01-07 16:58:03 +0800

    link neovim dot files in new dir

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,3 +1,8 @@
+mkdir -p /home/dakai/.config/nvim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/coc-settings.json /home/dakai/.config/nvim/coc-settings.json
+ln -s /home/dakai/setup_new_fedora_PC/nvim/coc.vim /home/dakai/.config/nvim/coc.vim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/init.vim /home/dakai/.config/nvim/init.vim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/plug.vim /home/dakai/.config/nvim/plug.vim
 #Check if user is root
 if [ "$EUID" -ne 0 ]
 then
@@ -13,9 +18,9 @@ fi
 #curl -fLo /root/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #ln -s /home/dakai/setup_new_fedora_PC/vimrc /root/.vimrc
 #
-##Setup neovim
-#mkdir -p /root/.config/nvim
-#ln -s /home/dakai/setup_new_fedora_PC/coc-settings.json /root/.config/nvim/coc-settings.json
-#ln -s /home/dakai/setup_new_fedora_PC/coc.vim /root/.config/nvim/coc.vim
-#ln -s /home/dakai/setup_new_fedora_PC/init.vim /root/.config/nvim/init.vim
-#ln -s /home/dakai/setup_new_fedora_PC/plug.vim /root/.config/nvim/plug.vim
+#Setup neovim
+mkdir -p /root/.config/nvim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/coc-settings.json /root/.config/nvim/coc-settings.json
+ln -s /home/dakai/setup_new_fedora_PC/nvim/coc.vim /root/.config/nvim/coc.vim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/init.vim /root/.config/nvim/init.vim
+ln -s /home/dakai/setup_new_fedora_PC/nvim/plug.vim /root/.config/nvim/plug.vim
