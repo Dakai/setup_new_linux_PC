82e5f9a9b325b0601f8817d628cd0de7a1eccb92
dakai <oudakai@gmail.com>
2023-06-17 19:15:10 +0800

    lsp save autoformatting works

--- lua_nvim/plugin/lspconfig.lua
+++ lua_nvim/plugin/lspconfig.lua
@@ -1,11 +1,9 @@
 local status, nvim_lsp = pcall(require, 'lspconfig')
 if (not status) then return end
 --
-local protocol = require('vim.lsp.protocol')
-
 -- https://levelup.gitconnected.com/a-step-by-step-guide-to-configuring-lsp-in-neovim-for-coding-in-next-js-a052f500da2#5f0e
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#show-line-diagnostics-automatically-in-hover-window
--- signs at line number 
+-- signs at line number
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
@@ -16,34 +14,38 @@ end
 vim.o.updatetime = 250
 vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
+local protocol = require('vim.lsp.protocol')
+
 local on_attach = function(client, bufnr)
-  -- formatting
+  -- format on save
   if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    --vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[autogroup END]]
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.format({ async = false }) end
+    })
   end
 end
---
---nvim_lsp.tsserver.setup {
---  on_attach = on_attach,
---  filetypes = {'typescript', 'typescriptreact', 'typescript.tsx'},
---  cmd = { "typescript-language-server", "--stdio"}
---}
---
+
+-- TypeScript
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" }
+}
+
 nvim_lsp.lua_ls.setup {
   on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
         -- Get the language server to recognize the 'vim' global
-        globals = {'vim'}
+        globals = { 'vim' }
       },
 
       workspace = {
         -- Make the server aware of Neovim runtime
-        library = vim.api.nvim_get_runtime_file("", true);
+        library = vim.api.nvim_get_runtime_file("", true),
       }
     }
   }
