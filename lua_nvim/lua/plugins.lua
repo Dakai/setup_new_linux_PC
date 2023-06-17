44e1d839cd1282c22c02ec14c48f6dccf6ddc4d7
dakai <oudakai@gmail.com>
2023-06-17 19:49:09 +0800

    tab and enter to select snips

--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -40,12 +40,13 @@ require('lazy').setup({
       { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },
-      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'onsails/lspkind-nvim' },
       { 'L3MON4D3/LuaSnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/nvim-cmp' },
     }
   },
-  { 'onsails/lspkind-nvim' },
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
