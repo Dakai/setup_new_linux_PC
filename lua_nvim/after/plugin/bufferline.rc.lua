f8510aade930317684c8c8c457291f5963a8918c
dakai <oudakai@gmail.com>
2023-06-19 11:25:04 +0800

    styling bufferline

--- lua_nvim/after/plugin/bufferline.rc.lua
+++ lua_nvim/after/plugin/bufferline.rc.lua
@@ -4,35 +4,36 @@ if (not status) then return end
 bufferline.setup({
   options = {
     mode = "tabs",
-    separator_style = 'slant',
+    separator_style = { '', '' },
     always_show_bufferline = true,
     show_buffer_close_icons = false,
     show_close_icon = false,
-    color_icons = true
-  },
-  highlights = {
-    separator = {
-      fg = '#100E23', -- separator background
-      -- bg = '#002b36',
-      bg = '#100E23', -- separator color
+    color_icons = true,
+    offsets = { {
+      filetype = "NvimTree",
+      text = "File Explorer",
+      highlight = "Directory",
+      text_align = "left"
+    } },
+    highlights = {
+      separator = {
+        fg = '#9feed5', -- separator background
+        bg = '#100E23', -- left separator background
+      },
+      background = {
+        fg = '#fdf6e3', -- not seleceted font color
+        bg = '#100E23' -- not selected background color
+      },
+      buffer_selected = {
+        fg = '#100E23', -- tab font
+        bg = '#9feed5', -- selected background color
+        bold = true,
+      },
+      fill = {
+        bg = '#100e23' -- whole background
+      }
     },
-    separator_selected = {
-      fg = '#100E23', -- selected buffer background
-      bg = '#9feed5', -- separator color
-    },
-    background = {
-      fg = '#fdf6e3', -- not seleceted font color
-      bg = '#100E23'  -- not selected background color
-    },
-    buffer_selected = {
-      fg = '#100E23', -- tab font
-      bg = '#9feed5', -- selected background color
-      bold = true,
-    },
-    fill = {
-      bg = '#100e23' -- whole background
-    }
-  },
+  }
 })
 -- vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 -- vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
