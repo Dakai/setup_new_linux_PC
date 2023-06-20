191bcf6e924390e3a40c990dec5e3981005a0cc5
dakai <oudakai@gmail.com>
2023-06-20 13:57:12 +0800

    markdown preview works

--- lua_nvim/lua/maps.lua
+++ lua_nvim/lua/maps.lua
@@ -8,16 +8,16 @@ keymap.set('n', '+', "<C-a>")
 keymap.set('n', '-', "<C-x>")
 
 -- Delete a word backwords
-keymap.set('n', 'dw','vb"_d')
+keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
 keymap.set('n', '<C-a>', 'g<S-v>G')
 
 -- New tab
-keymap.set('n', 'te', ':tabedit<Return>', {silent = true})
+keymap.set('n', 'te', ':tabedit<Return>', { silent = true })
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w', {silent = true})
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w', {silent = true})
+keymap.set('n', 'ss', ':split<Return><C-w>w', { silent = true })
+keymap.set('n', 'sv', ':vsplit<Return><C-w>w', { silent = true })
 -- Move window
 keymap.set('n', 'sj', '<C-w>h') -- left
 keymap.set('n', 'sl', '<C-w>l') -- right
@@ -25,13 +25,15 @@ keymap.set('n', 'si', '<C-w>k') -- up
 keymap.set('n', 'sk', '<C-w>j') -- down
 
 -- Resize window
-keymap.set('n','<C-w>k', '<C-w><')
-keymap.set('n','<C-w>i', '<C-w>>')
-keymap.set('n','<C-w><up>', '<C-w>+')
-keymap.set('n','<C-w><down>', '<C-w>-')
+keymap.set('n', '<C-w>k', '<C-w><')
+keymap.set('n', '<C-w>i', '<C-w>>')
+keymap.set('n', '<C-w><up>', '<C-w>+')
+keymap.set('n', '<C-w><down>', '<C-w>-')
 
 -- map <Space>w to save file
 vim.api.nvim_set_keymap('n', '<Space>w', ':w<CR>', { noremap = true })
 
 -- map <Space>q to quit neovim
 vim.api.nvim_set_keymap('n', '<Space>q', ':q<CR>', { noremap = true })
+
+vim.api.nvim_set_keymap('n', '<Space>m', '<Plug>MarkdownPreviewToggle', {})
--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -1,95 +1,103 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- Auto-install lazy.nvim if not present
 if not vim.loop.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
+  print("Installing lazy.nvim....")
   vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
     lazypath,
   })
-  print('Done.')
+  print("Done.")
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({
-  { 'xiyaowong/nvim-transparent' },
-  { 'nvim-lua/plenary.nvim' },
+require("lazy").setup({
+  { "xiyaowong/nvim-transparent" },
+  { "nvim-lua/plenary.nvim" },
   {
-    'dakai/embark-theme-vim',
+    "dakai/embark-theme-vim",
     config = function()
-      vim.cmd('colorscheme embark')
-    end
+      vim.cmd("colorscheme embark")
+    end,
   },
   {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-  { 'Xuyuanp/nerdtree-git-plugin' },
+  { "Xuyuanp/nerdtree-git-plugin" },
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
     dependencies = {
       -- LSP Support
-      { 'neovim/nvim-lspconfig' },
+      { "neovim/nvim-lspconfig" },
       {
-        'williamboman/mason.nvim',
+        "williamboman/mason.nvim",
         build = function()
-          vim.cmd('MasonUpdate')
+          vim.cmd("MasonUpdate")
         end,
       },
-      { 'williamboman/mason-lspconfig.nvim' },
+      { "williamboman/mason-lspconfig.nvim" },
 
       -- Autocompletion
-      { 'onsails/lspkind-nvim' },
-      { 'L3MON4D3/LuaSnip' },
-      { 'hrsh7th/cmp-nvim-lsp' },
-      { 'hrsh7th/cmp-buffer' },
-      { 'hrsh7th/cmp-path' },
-      { 'hrsh7th/nvim-cmp' },
-      {'rafamadriz/friendly-snippets'}
-    }
+      { "onsails/lspkind-nvim" },
+      { "L3MON4D3/LuaSnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "hrsh7th/nvim-cmp" },
+      { "rafamadriz/friendly-snippets" },
+    },
   },
   {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
-    }
+    },
   },
-  { 'nvim-tree/nvim-web-devicons' },
-  { 'nvim-treesitter/nvim-treesitter' },
-  { 'windwp/nvim-ts-autotag' },
-  { 'windwp/nvim-autopairs' },
-  { 'nvim-telescope/telescope.nvim' },
-  { 'nvim-telescope/telescope-file-browser.nvim' },
+  { "nvim-tree/nvim-web-devicons" },
+  { "nvim-treesitter/nvim-treesitter" },
+  { "windwp/nvim-ts-autotag" },
+  { "windwp/nvim-autopairs" },
+  { "nvim-telescope/telescope.nvim" },
+  { "nvim-telescope/telescope-file-browser.nvim" },
   {
     "willothy/nvim-cokeline",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    config = true
+    config = true,
   },
   { "lukas-reineke/indent-blankline.nvim" },
-  { 'karb94/neoscroll.nvim', },
+  { "karb94/neoscroll.nvim" },
   {
-    'Exafunction/codeium.vim',
+    "Exafunction/codeium.vim",
     config = function()
       vim.g.codeium_no_map_tab = 1
-      vim.keymap.set('i', '<M-l>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<M-i>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<M-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<M-j>', function() return vim.fn['codeium#Complete']() end, { expr = true })
-    end
+      vim.keymap.set("i", "<M-l>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+      vim.keymap.set("i", "<M-i>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<M-k>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<M-j>", function()
+        return vim.fn["codeium#Complete"]()
+      end, { expr = true })
+    end,
   },
   --{ 'davidosomething/format-ts-errors.nvim' },
-  { 'jose-elias-alvarez/null-ls.nvim' },
-  { 'MunifTanjim/prettier.nvim' },
-  { 'lewis6991/gitsigns.nvim' },
+  { "jose-elias-alvarez/null-ls.nvim" },
+  { "MunifTanjim/prettier.nvim" },
+  { "lewis6991/gitsigns.nvim" },
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
@@ -98,6 +106,15 @@ require('lazy').setup({
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
       })
-    end
-  }
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 })
