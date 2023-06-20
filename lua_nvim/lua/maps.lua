a13f13632f379e104005a0862554ac4c6738a261
dakai <oudakai@gmail.com>
2023-06-20 20:43:12 +0800

    gt to next tab & gT to prev tab

--- lua_nvim/lua/maps.lua
+++ lua_nvim/lua/maps.lua
@@ -55,3 +55,9 @@ vim.api.nvim_set_keymap(
 -- move highlighted text up and down"
 vim.api.nvim_set_keymap("v", "K", ":m '<-2<CR>gv==jgvo<esc>=jgvo", {})
 vim.api.nvim_set_keymap("v", "J", ":m '>+1<CR>gv==kgvo<esc>=kgvo", {})
+
+-- Map gt to next tab
+vim.api.nvim_set_keymap('n', 'gt', ':tabnext<CR>', { noremap = true, silent = true })
+
+-- Map gT to previous tab
+vim.api.nvim_set_keymap('n', 'gT', ':tabprev<CR>', { noremap = true, silent = true })
