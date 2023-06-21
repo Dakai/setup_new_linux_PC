92b87839da3f0347d8e9282856f18c37543d3081
dakai <oudakai@gmail.com>
2023-06-21 11:15:08 +0800

    null-ls format on save from async to sync

--- lua_nvim/after/plugin/null-ls.rc.lua
+++ lua_nvim/after/plugin/null-ls.rc.lua
@@ -3,38 +3,7 @@ if not status then
   return
 end
 
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(
-    bufnr,
-    "textDocument/formatting",
-    vim.lsp.util.make_formatting_params({}),
-    function(err, res, ctx)
-      if err then
-        local err_msg = type(err) == "string" and err or err.message
-        -- you can modify the log message / level (or ignore it completely)
-        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-        return
-      end
-
-      -- don't apply results if buffer is unloaded or has been modified
-      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-        return
-      end
-
-      if res then
-        local client = vim.lsp.get_client_by_id(ctx.client_id)
-        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-        vim.api.nvim_buf_call(bufnr, function()
-          vim.cmd("silent noautocmd update")
-        end)
-      end
-    end
-  )
-end
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 null_ls.setup({
   sources = {
     -- null_ls.builtins.diagnostics.eslint_d,
@@ -43,16 +12,24 @@ null_ls.setup({
     null_ls.builtins.formatting.phpcbf,
   },
   debug = false,
+
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePost", {
+      vim.api.nvim_create_autocmd("BufWritePre", {
         group = augroup,
         buffer = bufnr,
         callback = function()
-          async_formatting(bufnr)
+          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+          -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+          vim.lsp.buf.formatting_sync()
         end,
       })
     end
   end,
+
+})
+
+require("null-ls").setup({
+  -- you can reuse a shared lspconfig on_attach callback here
 })
