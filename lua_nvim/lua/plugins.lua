fd3043ec30def97e4962b4e7de6fa2f90994e742
dakai <oudakai@gmail.com>
2023-06-22 13:11:16 +0800

    setup telescope smart-open

--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -142,4 +142,18 @@ require("lazy").setup({
 			require("colorizer").setup()
 		end,
 	},
+	{
+		"danielfalk/smart-open.nvim",
+		branch = "0.2.x",
+		config = function()
+			require("telescope").load_extension("smart_open")
+		end,
+		dependencies = {
+			"kkharji/sqlite.lua",
+			-- Only required if using match_algorithm fzf
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			-- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+			{ "nvim-telescope/telescope-fzy-native.nvim" },
+		},
+	},
 })
