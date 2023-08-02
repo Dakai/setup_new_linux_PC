568724935e5655a531e7570f223deed90a6d97a7
dakai <oudakai@gmail.com>
2023-08-02 16:43:09 +0800

    Update telescope settings and keymaps for file browsing and searching

--- lua_nvim/after/plugin/telescope.rc.lua
+++ lua_nvim/after/plugin/telescope.rc.lua
@@ -1,157 +1,156 @@
 local status, telescope = pcall(require, "telescope")
 if not status then
-	return
+  return
 end
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
 
 local function telescope_buffer_dir()
-	return vim.fn.expand("%:p:h")
+  return vim.fn.expand("%:p:h")
 end
 
 local fb_actions = require("telescope").extensions.file_browser.actions
 
 telescope.setup({
-	defaults = {
-		mappings = {
-			n = {
-				["q"] = actions.close,
-			},
-		},
-	},
-	extensions = {
-		file_browser = {
-			theme = "dropdown",
-			-- disables netrw and use telescope-file-browser in its place
-			hijack_netrw = true,
-			mappings = {
-				-- your custom insert mode mappings
-				["i"] = {
-					["<C-w>"] = function()
-						vim.cmd("normal vbd")
-					end,
-				},
-				["n"] = {
-					-- your custom normal mode mappings
-					["N"] = fb_actions.create,
-					["h"] = fb_actions.goto_parent_dir,
-					["/"] = function()
-						vim.cmd("startinsert")
-					end,
-				},
-			},
-		},
-	},
+  defaults = {
+    mappings = {
+      n = {
+        ["q"] = actions.close,
+      },
+    },
+  },
+  extensions = {
+    file_browser = {
+      theme = "dropdown",
+      -- disables netrw and use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        -- your custom insert mode mappings
+        ["i"] = {
+          ["<C-w>"] = function()
+            vim.cmd("normal vbd")
+          end,
+        },
+        ["n"] = {
+          -- your custom normal mode mappings
+          ["N"] = fb_actions.create,
+          ["h"] = fb_actions.goto_parent_dir,
+          ["/"] = function()
+            vim.cmd("startinsert")
+          end,
+        },
+      },
+    },
+  },
 })
 
 telescope.load_extension("file_browser")
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<Space>s",
-	"<Cmd>lua require('telescope').extensions.smart_open.smart_open()<CR>",
-	{ noremap = true, silent = true }
+  "n",
+  "<Space>s",
+  "<Cmd>lua require('telescope').extensions.smart_open.smart_open()<CR>",
+  { noremap = true, silent = true }
 )
 
 vim.keymap.set("n", ";f", function()
-	builtin.find_files({
-		respect_gitignore = false,
-		no_ignore = true,
-		hidden = true,
-		file_ignore_patterns = {
-			"node_modules",
-			".git",
-			".next",
-			".lock",
-			"package-lock.json",
-			".jpg",
-			"jpeg",
-			"png",
-			"PNG",
-			"JPEG",
-			"JPG",
-			"ico",
-		},
-	})
+  builtin.find_files({
+    respect_gitignore = false,
+    no_ignore = true,
+    hidden = true,
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      ".next",
+      ".lock",
+      "package-lock.json",
+      ".jpg",
+      "jpeg",
+      "png",
+      "PNG",
+      "JPEG",
+      "JPG"
+    },
+  })
 end)
 vim.keymap.set("n", "<Space>t", function()
-	builtin.find_files({
-		respect_gitignore = false,
-		no_ignore = true,
-		hidden = true,
-		file_ignore_patterns = {
-			"node_modules",
-			".git",
-			".next",
-			".lock",
-			"package-lock.json",
-			".jpg",
-			"jpeg",
-			"png",
-			"PNG",
-			"JPEG",
-			"JPG",
-			"ico",
-		},
-	})
+  builtin.find_files({
+    respect_gitignore = false,
+    no_ignore = true,
+    hidden = true,
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      ".next",
+      ".lock",
+      "package-lock.json",
+      ".jpg",
+      "jpeg",
+      "png",
+      "PNG",
+      "JPEG",
+      "JPG",
+      "ico",
+    },
+  })
 end)
 vim.keymap.set("n", "<Space>g", function()
-	builtin.live_grep({
-		respect_gitignore = false,
-		no_ignore = true,
-		hidden = false,
-		file_ignore_patterns = {
-			"node_modules",
-			".git",
-			".next",
-			".lock",
-			"package-lock.json",
-			".jpg",
-			"jpeg",
-			"png",
-			"PNG",
-			"JPEG",
-			"JPG",
-			"ico",
-		},
-	})
+  builtin.live_grep({
+    respect_gitignore = false,
+    no_ignore = true,
+    hidden = false,
+    file_ignore_patterns = {
+      "node_modules",
+      ".git",
+      ".next",
+      ".lock",
+      "package-lock.json",
+      ".jpg",
+      "jpeg",
+      "png",
+      "PNG",
+      "JPEG",
+      "JPG",
+      "ico",
+    },
+  })
 end)
 vim.keymap.set("n", "<Space>b", function()
-	builtin.buffers()
+  builtin.buffers()
 end)
 vim.keymap.set("n", "<Space>h", function()
-	builtin.help_tags()
+  builtin.help_tags()
 end)
 vim.keymap.set("n", "<Space>r", function()
-	builtin.resume()
+  builtin.resume()
 end)
 vim.keymap.set("n", "<Space>e", function()
-	builtin.diagnostics()
+  builtin.diagnostics()
 end)
 vim.keymap.set("n", "sf", function()
-	telescope.extensions.file_browser.file_browser({
-		path = "%:p:h",
-		cwd = telescope_buffer_dir(),
-		respect_gitignore = false,
-		hidden = true,
-		grouped = true,
-		previewer = false,
-		initial_mode = "normal",
-		layout_config = { height = 40 },
-	})
+  telescope.extensions.file_browser.file_browser({
+    path = "%:p:h",
+    cwd = telescope_buffer_dir(),
+    respect_gitignore = false,
+    hidden = true,
+    grouped = true,
+    previewer = false,
+    initial_mode = "normal",
+    layout_config = { height = 40 },
+  })
 end)
 vim.keymap.set("n", ";r", function()
-	builtin.live_grep()
+  builtin.live_grep()
 end)
 vim.keymap.set("n", "\\\\", function()
-	builtin.buffers()
+  builtin.buffers()
 end)
 vim.keymap.set("n", ";h", function()
-	builtin.help_tags()
+  builtin.help_tags()
 end)
 vim.keymap.set("n", ";;", function()
-	builtin.resume()
+  builtin.resume()
 end)
 vim.keymap.set("n", ";e", function()
-	builtin.diagnostics()
+  builtin.diagnostics()
 end)
