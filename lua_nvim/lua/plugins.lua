216da10cfd55c507749e260ff2634003e5fdae7b
dakai <oudakai@gmail.com>
2023-10-13 23:08:23 +0800

    Add event listener for codeium.vim plugin

--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -77,6 +77,7 @@ require("lazy").setup({
   { "karb94/neoscroll.nvim" },
   {
     "Exafunction/codeium.vim",
+    event = 'BufEnter',
     config = function()
       vim.g.codeium_no_map_tab = 1
       vim.keymap.set("i", "<M-l>", function()
