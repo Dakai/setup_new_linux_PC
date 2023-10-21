df5f68affcf4ee20c8a52c2c304f70518ecad6c1
dakai <oudakai@gmail.com>
2023-10-21 18:15:00 +0800

    Update LuaSnip plugin to latest release and add dependencies

--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -44,12 +44,20 @@ require("lazy").setup({
 
       -- Autocompletion
       { "onsails/lspkind-nvim" },
-      { "L3MON4D3/LuaSnip" },
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/nvim-cmp" },
-      { "rafamadriz/friendly-snippets" },
+      --{ "rafamadriz/friendly-snippets" },
     },
   },
   {
