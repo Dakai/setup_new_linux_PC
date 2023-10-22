9c78b8b57e3c8a62026ba98ce8e66d34e2066e91
dakai <oudakai@gmail.com>
2023-10-23 05:10:40 +0800

    Update LSP zero configuration and color settings

--- lua_nvim/after/plugin/lsp-zero.rc.lua
+++ lua_nvim/after/plugin/lsp-zero.rc.lua
@@ -31,6 +31,7 @@ cmp.setup({
   completion = {
     completeopt = 'menu,menuone,noinsert'
   },
+
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = cmp.mapping.select_next_item(),
     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
@@ -57,7 +58,7 @@ cmp.setup({
     },
     format = lspkind.cmp_format({
       mode = 'symbol_text',  -- show only symbol annotations
-      maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      maxwidth = 80,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       menu = {
         buffer = "[BUF]",
