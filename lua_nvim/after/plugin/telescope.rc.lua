a1b6df370310189af4e9dbd4515b13b87a98e213
dakai <oudakai@gmail.com>
2023-10-23 05:45:04 +0800

    Prevent insert mode when opening file from telescope

--- lua_nvim/after/plugin/telescope.rc.lua
+++ lua_nvim/after/plugin/telescope.rc.lua
@@ -154,3 +154,13 @@ vim.keymap.set("n", ";e", function()
 end)
 -- Show Todos in Trouble
 vim.keymap.set("n", ";t", ":TodoTelescope<Return>", { silent = true })
+
+-- prevent insert mode when open file from telescope
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1561836585
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
