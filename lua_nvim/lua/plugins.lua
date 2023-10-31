2283e0c02b52c45a045306acfefa90d4da6592f5
dakai <oudakai@gmail.com>
2023-11-01 05:39:43 +0800

    Add neorg plugin and setup neorg configuration

--- lua_nvim/lua/plugins.lua
+++ lua_nvim/lua/plugins.lua
@@ -260,5 +260,25 @@ require("lazy").setup({
 
       }
     end,
+  },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {},  -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = {      -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Sync/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
   }
 })
