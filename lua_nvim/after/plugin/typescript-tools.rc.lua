62ba2ce9170abdcbd581be892fbd2c77d6feb057
dakai <oudakai@gmail.com>
2023-11-03 01:22:26 +0800

    Fix tmux.conf and typescript-tools.rc.lua configurations

--- lua_nvim/after/plugin/typescript-tools.rc.lua
+++ lua_nvim/after/plugin/typescript-tools.rc.lua
@@ -1,22 +1,22 @@
-local status, ts_tools = pcall(require, "typscript-tools")
-
-if not status then
-  return
-end
-
-local api = require("typescript-tools.api")
-
-ts_tools.setup {
-  filetypes = { "typescript", "javascript", "svelte" },
-  on_attach = function(client, bufnr)
-    if vim.bo[bufnr].filetype == "svelte" then
-      client.stop()
-    end
-  end,
-  handlers = {
-    ["textDocument/publishDiagnostics"] = api.filter_diagnostics(
-    -- Ignore 'CommonJS convert' diagnostics.
-      { 80001 }
-    ),
-  },
-}
+--local status, ts_tools = pcall(require, "typscript-tools")
+--
+--if not status then
+--  return
+--end
+--
+--local api = require("typescript-tools.api")
+--
+--ts_tools.setup {
+--  filetypes = { "typescript", "javascript", "svelte" },
+--  on_attach = function(client, bufnr)
+--    if vim.bo[bufnr].filetype == "svelte" then
+--      client.stop()
+--    end
+--  end,
+--  handlers = {
+--    ["textDocument/publishDiagnostics"] = api.filter_diagnostics(
+--    -- Ignore 'CommonJS convert' diagnostics.
+--      { 80001 }
+--    ),
+--  },
+--}
