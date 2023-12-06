0a8bb8fe96eb7f295a9a0873ce849ba08962548f
dakai <oudakai@gmail.com>
2023-12-06 11:23:45 +0800

    Add yamlfmt as a formatter for yaml files

--- lua_nvim/after/plugin/format-on-save.rc.lua
+++ lua_nvim/after/plugin/format-on-save.rc.lua
@@ -1,6 +1,11 @@
 local format_on_save = require("format-on-save")
 local formatters = require("format-on-save.formatters")
 
+function yamlfmt()
+  local fileName = vim.api.nvim_buf_get_name(0)
+  vim.cmd(":!yamlfmt " .. fileName)
+end
+
 format_on_save.setup({
   exclude_path_patterns = {
     "/node_modules/",
@@ -28,8 +33,8 @@ format_on_save.setup({
     terraform = formatters.lsp,
     typescript = formatters.prettierd,
     typescriptreact = formatters.prettierd,
-    yaml = formatters.lsp,
-
+    -- yaml = formatters.lsp,
+    yaml = yamlfmt,
     -- Optional: fallback formatter to use when no formatters match the current filetype
     fallback_formatter = {
       formatters.remove_trailing_whitespace,
